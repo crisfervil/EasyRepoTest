f65cb77ff5b982dcf1faa8c03cca1f2f95fe77b2
Cristhian Fernandez <crisfervil@gmail.com>
2018-07-12 06:37:24 +0100

    Added global search test

---
+++ src/EasyReproTest/GlobalSearchTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Dynamics365.UIAutomation.Api;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EasyReproTest
+{
+    public class GlobalSearchTest
+    {
+        [Fact]
+        public void WEBTestGlobalSearch()
+        {
+            using (var xrmBrowser = new Browser(BrowserType.Chrome))
+            {
+                var urlStr = Environment.GetEnvironmentVariable("D365_URL") ?? "http://acme.crm.dynamics.com";
+                var userNameStr = Environment.GetEnvironmentVariable("D365_USER") ?? "admin@acme.onmicrosoft.com";
+                var pwdStr = Environment.GetEnvironmentVariable("D365_PWD") ?? "Password@12345";
+
+                var url = new Uri(urlStr);
+                var userName = userNameStr.ToSecureString();
+                var pwd = pwdStr.ToSecureString();
+
+                xrmBrowser.LoginPage.Login(url, userName, pwd);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+
+                xrmBrowser.ThinkTime(500);
+
+                xrmBrowser.Navigation.GlobalSearch("contoso");
+
+                xrmBrowser.GlobalSearch.Search("Contoso");
+                xrmBrowser.ThinkTime(4000);
+            }
+        }
+    }
+}
